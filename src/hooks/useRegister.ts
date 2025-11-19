cfba1ce2f8e854b65a53aef82a4cab04b66c938f
Vlad <vladislav.tihonov@innowise.com>
2025-11-19 19:24:12 +0300

    refactor(auth): extract register logic to useRegister hook

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { AuthFormValues } from "../components/AuthForm";
+import { RegisterResponse } from "../types/auth.types";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const registerRequest = async (
+  values: AuthFormValues
+): Promise<RegisterResponse> => {
+  const response = await axios.post(
+    `${import.meta.env.VITE_AUTH_API_URL}/register`,
+    {
+      username: values.username,
+      email: values.email,
+      password: values.password,
+    }
+  );
+
+  return response.data;
+};
+
+export const useRegister = () => {
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: registerRequest,
+    onSuccess: (data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("refreshToken", data.refreshToken);
+      navigate("/");
+    },
+    onError: (error) => {
+      console.error("Register failed:", error);
+    },
+  });
+};
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,49 +1,20 @@
-import axios from "axios";
-import AuthForm, { type AuthFormValues } from "../components/AuthForm";
-import { useNavigate } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
-import { RegisterResponse } from "../types/auth.types";
+import AuthForm from "../components/AuthForm";
 import type { JSX } from "react";
-
-const registerRequest = async (
-  values: AuthFormValues
-): Promise<RegisterResponse> => {
-  const { email, password, username } = values;
-  const response = await axios.post(
-    `${import.meta.env.VITE_AUTH_API_URL}/register`,
-    { email, password, username }
-  );
-  return response.data;
-};
+import { useRegister } from "../hooks/useRegister";
 
 function RegisterPage(): JSX.Element {
-  const navigate = useNavigate();
-
-  const mutation = useMutation({
-    mutationFn: registerRequest,
-    onSuccess: (data) => {
-      localStorage.setItem("accessToken", data.accessToken);
-      localStorage.setItem("refreshToken", data.refreshToken);
-      navigate("/");
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-
-  const handleRegisterSubmit = (values: AuthFormValues) => {
-    mutation.mutate(values);
-  };
+  const { mutate: registerMutation, isPending } = useRegister();
 
   return (
     <AuthForm
       title='Register'
       buttonText='Register'
-      onSubmit={handleRegisterSubmit}
+      onSubmit={registerMutation}
       showUsername={true}
       footerText='Есть аккаунт?'
       footerLinkText='Вход'
       footerLinkTo='/login'
+      isLoading={isPending}
     />
   );
 }
