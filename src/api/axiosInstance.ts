60f4a135e49e61eb97e16e21b642b29a932aadb5
Vlad <vladislav.tihonov@innowise.com>
2025-12-10 23:42:54 +0300

    feat(api): add axios interceptor for auto token refresh

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,35 @@
-import axios from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _isRetry?: boolean;
+}
 
 export const authApi = axios.create({
   baseURL: import.meta.env.VITE_AUTH_API_URL,
   withCredentials: true,
 });
+
+authApi.interceptors.request.use(
+  (response) => response,
+
+  async (error: AxiosError) => {
+    const originalRequest = error.config as CustomAxiosRequestConfig;
+
+    if (
+      error.response?.status === 401 &&
+      originalRequest &&
+      !originalRequest._isRetry
+    ) {
+      originalRequest._isRetry = true;
+
+      try {
+        await authApi.post("/auth/refresh");
+        return authApi.request(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
